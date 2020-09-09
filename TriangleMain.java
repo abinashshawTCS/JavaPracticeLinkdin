b6b36c789e5983776ba3553f70ca9616f3ecd7b3
abinashshawTCS <70328149+abinashshawTCS@users.noreply.github.com>
2020-09-09 15:31:11 +0530

    Add files via upload

---
+++ TriangleMain.java
@@ -0,0 +1,24 @@
+class TriangleMain{
+
+  public static void main(String[] args){
+    /*Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+    Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
+
+    double triangleAArea = triangleA.findArea();
+    System.out.println(triangleAArea);
+
+    double triangleBArea = triangleB.findArea();
+    System.out.println(triangleBArea);
+
+    System.out.println(triangleA.sideLenOne); //non-static reference
+    System.out.println(triangleB.base);
+
+    System.out.println(Triangle.noOfSides);  //Static reference
+
+    System.out.println(Math.pow(5, 8));
+    System.out.println(Math.abs(-100));
+    */
+
+    new Student
+  }
+}
